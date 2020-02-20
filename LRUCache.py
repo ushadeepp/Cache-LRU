2c5f39dc1daae56b8c2db037534ae4d1aec18d08
ushadeepp <ushadeep123@gmail.com>
2020-02-20 16:23:02 +0530

    Updated the Names

--- LRUCache.py
+++ LRUCache.py
@@ -2,36 +2,36 @@ from Node import *
 from LinkedList import *
 from CachedDataQueue import * 
 
-#  HashTable 
-class HashTable:
+#  LRUCache data structure is the combination of the HashTable which stores data in single Linked List & Doubly Linked List
+class LRUCache:
     
     # cunstrcutor initialize tableSize & cacheMaxSize
     def __init__(self,tableSize,cacheMaxSize):
         self.tableSize=tableSize
         self.cacheMaxSize=cacheMaxSize
-        self.table=[]
+        self.hashTable=[]
         for i in range(tableSize):
-            self.table.append(LinkedList())
+            self.hashTable.append(LinkedList())
         self.cacheData=CachedDataQueue()
     
     #  considering the keys are only int caliculating the hash of key
     def hashFunc(self,key):
         return key%self.tableSize
 
-    #  Insert data in hashtable
+    #  Insert data in LRUCache
     def insertData(self,key,data):
         if self.cacheData.getSize()==self.cacheMaxSize:
             self.evictLastElement()
         keyHash=self.hashFunc(key)
         node=Node(key,data,None,None)
         self.cacheData.addData(node)
-        self.table[keyHash].addData(node)
+        self.hashTable[keyHash].addData(node)
 
 
     #  get data from cache
     def getData(self,key):
         keyHash=self.hashFunc(key)
-        li=self.table[keyHash]
+        li=self.hashTable[keyHash]
         result=li.search(key)
         if(result==None):
             return None
@@ -42,7 +42,7 @@ class HashTable:
     def evictLastElement(self):
         deletedNode=self.cacheData.getTail()
         keyHash=self.hashFunc(deletedNode.getKey())
-        self.table[keyHash].deleteNode(deletedNode)
+        self.hashTable[keyHash].deleteNode(deletedNode)
         self.cacheData.deleteTail()
         
 
@@ -50,7 +50,7 @@ class HashTable:
     def printTableAndCacheQueue(self):
         for key in range(self.tableSize):
             print('Key'+str(key)+':   --------------------')
-            self.table[key].printData()        
+            self.hashTable[key].printData()        
         self.cacheData.printWholeList()
 
         
